4b0925d98d613e324118aa7495ca8df31113eacb
mohamed SABER <mohamedsaber@MacBook-de-mohamed.local>
2020-07-24 14:41:57 +0200

    Création du header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CocktailsListComponent,
+    CocktailDetailsComponent
   ],
   imports: [
     BrowserModule
